786840e872b4050a86bf98f6a2c8bfafac3d1bdf
MacStudent <macstudent@128-imac24.local>
2018-11-13 09:02:31 -0500

    pin

--- MApKitExample/ViewController.swift
+++ MApKitExample/ViewController.swift
@@ -14,13 +14,37 @@ class ViewController: UIViewController {
     @IBOutlet weak var mapkit: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        //example 1 : sho
         // Do any additional setup after loading the view, typically from a nib.
-        let x = MKCoordinateRegionMake(<#T##centerCoordinate: CLLocationCoordinate2D##CLLocationCoordinate2D#>, <#T##span: MKCoordinateSpan##MKCoordinateSpan#>)
-        let zoom = MKCoordinateSpanMake(<#T##latitudeDelta: CLLocationDegrees##CLLocationDegrees#>, <#T##longitudeDelta: CLLocationDegrees##CLLocationDegrees#>)
+        let x = CLLocationCoordinate2DMake(43.6532, -79.3832)
+        
+        //pick a zoom level
+        let y = MKCoordinateSpanMake(0.1, 0.1)
+        
+        //set the region property
+        let z = MKCoordinateRegionMake(x, y)
+        self.mapkit.setRegion(z, animated: true)
         
         
+        // example 2
         
         
+        // craete a pin
+        let pin = MKPointAnnotation()
+        //set the latitude and longitude
+        pin.coordinate = x
+    
+        // add a information on popup
+        pin.title = " here is some information "
+        
+        //show the pin on the map
+        self.mapkit.addAnnotation(pin)
+        
+        // second pin
+        let pin2 = MKPointAnnotation()
+        pin.coordinate = CLLocationCoordinate2DMake(43.65644, -79.3807)
+        pin.title = "second pin"
+        self.mapkit.addAnnotation(pin2)
         
     }
 
