509395f8466e8b2e5bf93b451bfcd69588f34c6e
MacStudent <macstudent@128-imac24.local>
2018-11-14 10:38:16 -0500

    Search screen

---
+++ MApKitExample/SearchViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SearchViewController.swift
+//  MApKitExample
+//
+//  Created by MacStudent on 2018-11-14.
+//  Copyright © 2018 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    @IBOutlet weak var txtSearch: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func btnSearch(_ sender: Any) {
+    }
+    
+}
